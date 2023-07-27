bf12a9353348c1e2ad6a8e45b81c30cd53d243c8
Elena <pumpkinpum@gmail.com>
2023-07-27 11:12:08 +0300

    fix: add require

--- components/ui/ModalLabel.js
+++ components/ui/ModalLabel.js
@@ -2,7 +2,7 @@ export default function ModalLabel({htmlFor, text, required}) {
   return (
     <label className='pb-2 text-fiord text-sl' htmlFor={htmlFor}>
       {text}
-      {required&&<span className="red">*</span>}
+      {required&&<span className="text-mandy ml-1.5">*</span>}
     </label>
   );
 }
