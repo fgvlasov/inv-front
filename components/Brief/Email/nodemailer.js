25b1396e4d1331fe4e6a272cf46e91c272ba8dcb
fgvlasov <fgvlasov@gmail.com>
2023-09-21 12:10:33 +0300

    email from Global

--- components/Brief/Email/nodemailer.js
+++ components/Brief/Email/nodemailer.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { createEmail } from "./createEmail";
+import { fetchAPI } from "lib/api";
 var nodemailer = require("nodemailer");
 
 const user = "invfront164@gmail.com";
@@ -15,10 +17,22 @@ export const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendMail = async (data) => {
+export async function SendMail(data) {
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const emailRes = await fetchAPI("/global", {
+        fields: ["Email_forms"],
+      });
+      console.log(emailRes);
+      setEmail(emailRes);
+    }
+    fetchData();
+  }, []);
+
   const result = await transporter.sendMail({
     subject: "Заявка",
-    to: "info@invert.studio",
+    to: email,
     html: createEmail(data),
   });
   const failed = result.rejected.concat(result.pending).filter(Boolean);
@@ -26,4 +40,4 @@ export const sendMail = async (data) => {
     throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`);
   }
   return result;
-};
+}
