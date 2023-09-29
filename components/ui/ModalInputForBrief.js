c72909d440fcfe5b7d52d0938c570211a0767ddb
fgvlasov <fgvlasov@gmail.com>
2023-09-29 12:37:01 +0300

    mask

--- components/ui/ModalInputForBrief.js
+++ components/ui/ModalInputForBrief.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { InputErrorMessage } from "./InputErrorMessage";
+import MaskedInput from "react-input-mask";
 
 export default function ModalInputForBrief({
   type,
@@ -10,18 +12,29 @@ export default function ModalInputForBrief({
   pattern,
   max = 50,
 }) {
+  const [tel, setTel] = useState("");
   return (
     <>
-      <input
-        className="py-3 px-5 w-full border border-link-water rounded-5xl
-      lg:max-w-[422px] lg:py-3.8"
-        type={type}
-        id={id}
-        placeholder={placeholder}
-        {...register(name, pattern)}
-        min={type === "number" ? 0 : undefined}
-        max={type === "number" ? max : undefined}
-      />
+      <MaskedInput
+        mask="+7 (921) 000-0000"
+        alwaysShowMask
+        onChange={(e) => setTel(e.target.value)}
+        value={tel}
+        name={name} // Fix: Use the correct prop name
+      >
+        {(inputProps) => (
+          <input
+            className="py-3 px-5 w-full border border-link-water rounded-5xl lg:max-w-[422px] lg:py-3.8"
+            type={type}
+            id={id}
+            placeholder={placeholder}
+            {...register(name, { pattern })} // Fix: Pass the pattern inside an object
+            {...inputProps}
+            min={type === "number" ? 0 : undefined}
+            max={type === "number" ? max : undefined}
+          />
+        )}
+      </MaskedInput>
       {error && <InputErrorMessage message={error} />}
     </>
   );
